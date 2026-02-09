c2ae8229c3033b7857890624dbbd434120e5bc02
Garima63005 <garimadabas1206@gmail.com>
2026-02-09 05:34:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const response = document.getElementById("response");
+
+yesBtn.addEventListener("click", () => {
+  response.innerHTML = "Yayyy! Toh ab officially mera Valentine ho gaya 😍💖<br>Movie date pending! 🎬🍿";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
